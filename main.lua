1ec3550c0131d4a2f29de5f65e7eb847ec59ecef
Teus-Scripts <mateusboaventura779@gmail.com>
2024-09-29 04:21:45 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Besty191/MAZI-API/main/Attack%20No%20Animation"))()
+
+local CombatFramework = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
+local Camera = require(game.ReplicatedStorage.Util.CameraShaker)
+Camera:Stop()
+coroutine.wrap(function()
+    game:GetService("RunService").Stepped:Connect(function()
+        if getupvalues(CombatFramework)[2]['activeController'].timeToNextAttack then
+            getupvalues(CombatFramework)[2]['activeController'].timeToNextAttack = 0
+            getupvalues(CombatFramework)[2]['activeController'].hitboxMagnitude = 25
+            getupvalues(CombatFramework)[2]['activeController']:attack()
+        end
+    end)
+end)()
