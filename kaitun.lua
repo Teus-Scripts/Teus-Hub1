b6fdfb8ebbfb42786c7dd4f2af53ae7021e8dae2
Teus-Scripts <mateusboaventura779@gmail.com>
2024-10-18 13:23:25 -0300

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,191 +1,165 @@
--- Gui to Lua
--- Version: 3.2
+print("[Teus/Info] Connecting To Server");
 
--- Instances:
+print("[Teus/Info] Connected To Server"); 
 
-local ChatSplitter = Instance.new("ScreenGui")
-local Frame = Instance.new("Frame")
-local ImageLabel = Instance.new("ImageLabel")
-local Title = Instance.new("TextLabel")
-local X = Instance.new("TextButton")
-local Frame_2 = Instance.new("Frame")
-local Chat = Instance.new("TextBox")
-local ChatBtn = Instance.new("TextButton")
-local DelayBox = Instance.new("TextBox")
-local minimze = Instance.new("TextButton")
+print("[Teus/Info] Fetching Requirements Data..."); 
 
---Properties:
+print("[Teus/Info] Saving Data To Workspace"); 
 
-ChatSplitter.Name = "ChatSplitter"
-ChatSplitter.Parent = game.CoreGui
-ChatSplitter.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-ChatSplitter.ResetOnSpawn = false
+print("[Teus/Info] Authencating..."); 
+wait(.2)
+print("[Teus/Info] Authencation Disabled, Loading Script...");
 
-Frame.Parent = ChatSplitter
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.BorderColor3 = Color3.fromRGB(255, 255, 255)
-Frame.LayoutOrder = 1
-Frame.Position = UDim2.new(0.302132696, 0, 0.0430440791, 0)
-Frame.Size = UDim2.new(0, 323, 0, 25)
+print("[Teus/Info] Loaded In "..tostring(tick()-gay).."ms")
 
-ImageLabel.Parent = Frame
+game: GetService"RunService".Heartbeat: Connect(function() 
+  sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge) 
+  pcall(function() 
+    for i, v in pairs(game.workspace.Enemies:GetChildren()) do 
+      if v.Humanoid.Health < 1 and not aD then 
+        v:Destroy() 
+      end 
+    end 
+  end)
+
+  pcall(function() 
+    if setfflag then
+        setfflag("AbuseReportScreenshot", "False")
+        setfflag("AbuseReportScreenshotPercentage", "0")
+    end 
+    end)
+end ) 
+  
+ScreenGui = Instance.new("ScreenGui");
+
+Frame = Instance.new("Frame");
+
+UIStroke = Instance.new("UIStroke");
+UIGradient = Instance.new("UIGradient");
+UICorner = Instance.new("UICorner");
+TextLabel = Instance.new("TextLabel");
+UIGradient1 = Instance.new("UIGradient");
+TextLabel1 = Instance.new("TextLabel");
+UIGradient2 = Instance.new("UIGradient");
+ImageLabel = Instance.new("ImageButton");
+adiadi = true
+ImageLabel.Activated: Connect(function () 
+    game:GetService"RunService": Set3dRenderingEnabled(not a)
+    adiadi = not a
+end) 
+
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+ScreenGui.Parent = game:GetService("CoreGui")
+
+Frame.AnchorPoint = Vector2.new(0.5, 0.5)
+Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BackgroundTransparency = 0 -- Transparência removida
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.499250829, 0, 0.181725562, 0)
+Frame.Size = UDim2.new(0, 511, 0, 76)
+Frame.Parent = ScreenGui
+
+UIStroke.Color = Color3.fromRGB(255, 255, 255)
+UIStroke.Thickness = 1.5
+UIStroke.Parent = Frame
+
+UIGradient.Color = ColorSequence.new{
+	ColorSequenceKeypoint.new(0, Color3.fromRGB(15.000000055879354, 255, 255)),
+	ColorSequenceKeypoint.new(1, Color3.fromRGB(207.00000286102295, 62.00000010430813, 255))
+}
+UIGradient.Parent = UIStroke
+
+UICorner.CornerRadius = UDim.new(0, 5)
+UICorner.Parent = Frame
+
+TextLabel.Font = Enum.Font.FredokaOne
+TextLabel.Text = "Heiskso Hub Kaitun / Cuttay"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 20
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 1
+TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.0199637525, 0, 0.276315689, 0)
+TextLabel.Size = UDim2.new(0, 489, 0, 11)
+TextLabel.Parent = Frame
+
+UIGradient1.Color = ColorSequence.new{
+	ColorSequenceKeypoint.new(0, Color3.fromRGB(15.000000055879354, 255, 255)),
+	ColorSequenceKeypoint.new(1, Color3.fromRGB(207.00000286102295, 62.00000010430813, 255))
+}
+UIGradient1.Parent = TextLabel
+
+TextLabel1.Font = Enum.Font.FredokaOne
+TextLabel1.Text = "Heiskso Script • .gg/yF4VEq7aDH"
+TextLabel1.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel1.TextSize = 20
+TextLabel1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel1.BackgroundTransparency = 1
+TextLabel1.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel1.BorderSizePixel = 0
+TextLabel1.Position = UDim2.new(0.0199637525, 0, 0.539473593, 0)
+TextLabel1.Size = UDim2.new(0, 489, 0, 22)
+TextLabel1.Parent = Frame
+
+UIGradient2.Color = ColorSequence.new{
+	ColorSequenceKeypoint.new(0, Color3.fromRGB(15.000000055879354, 255, 255)),
+	ColorSequenceKeypoint.new(1, Color3.fromRGB(207.00000286102295, 62.00000010430813, 255))
+}
+UIGradient2.Parent = TextLabel1
+
+ImageLabel.Image = "rbxassetid://15992619207"
 ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-ImageLabel.BorderColor3 = Color3.fromRGB(255, 255, 255)
-ImageLabel.Position = UDim2.new(0.0120000001, 0, 0.119999997, 0)
-ImageLabel.Size = UDim2.new(0, 19, 0, 19)
-ImageLabel.Image = "http://www.roblox.com/asset/?id=9645662961"
+ImageLabel.BackgroundTransparency = 1
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.448140889, 0, -0.473684222, 0)
+ImageLabel.Size = UDim2.new(0, 50, 0, 50)
+ImageLabel.Parent = Frame
 
-Title.Name = "Title"
-Title.Parent = Frame
-Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Title.BackgroundTransparency = 1.000
-Title.BorderColor3 = Color3.fromRGB(255, 255, 255)
-Title.Position = UDim2.new(0.0897832811, 0, 0, 0)
-Title.Size = UDim2.new(0, 136, 0, 25)
-Title.Font = Enum.Font.SourceSans
-Title.Text = "ChatSplitter - v0.1"
-Title.TextColor3 = Color3.fromRGB(0, 0, 0)
-Title.TextSize = 14.000
-Title.TextXAlignment = Enum.TextXAlignment.Left
-
-X.Name = "X"
-X.Parent = Frame
-X.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-X.BorderColor3 = Color3.fromRGB(255, 255, 255)
-X.Position = UDim2.new(0.836000025, 0, 0, 0)
-X.Size = UDim2.new(0, 51, 0, 23)
-X.Font = Enum.Font.Gotham
-X.Text = "X"
-X.TextColor3 = Color3.fromRGB(0, 0, 0)
-X.TextSize = 16.000
-
-Frame_2.Parent = Frame
-Frame_2.Active = true
-Frame_2.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
-Frame_2.BorderColor3 = Color3.fromRGB(45, 45, 45)
-Frame_2.Position = UDim2.new(-0.00127285696, 0, 1, 0)
-Frame_2.Size = UDim2.new(0, 323, 0, 107)
-
-Chat.Name = "Chat"
-Chat.Parent = Frame_2
-Chat.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-Chat.BorderColor3 = Color3.fromRGB(25, 25, 25)
-Chat.Position = UDim2.new(0.0247678012, 0, 0.120135188, 0)
-Chat.Size = UDim2.new(0, 192, 0, 41)
-Chat.Font = Enum.Font.SourceSans
-Chat.PlaceholderText = "Enter Your Chat Here."
-Chat.Text = ""
-Chat.TextColor3 = Color3.fromRGB(255, 255, 255)
-Chat.TextScaled = true
-Chat.TextSize = 14.000
-Chat.TextWrapped = true
-
-ChatBtn.Name = "ChatBtn"
-ChatBtn.Parent = Frame_2
-ChatBtn.BackgroundColor3 = Color3.fromRGB(27, 27, 27)
-ChatBtn.Position = UDim2.new(0.0247678012, 0, 0.600030422, 0)
-ChatBtn.Size = UDim2.new(0, 192, 0, 33)
-ChatBtn.Font = Enum.Font.SourceSans
-ChatBtn.Text = "Chat"
-ChatBtn.TextColor3 = Color3.fromRGB(255, 255, 255)
-ChatBtn.TextScaled = true
-ChatBtn.TextSize = 14.000
-ChatBtn.TextWrapped = true
-
-DelayBox.Name = "DelayBox"
-DelayBox.Parent = Frame_2
-DelayBox.BackgroundColor3 = Color3.fromRGB(25, 25, 25)
-DelayBox.BorderColor3 = Color3.fromRGB(25, 25, 25)
-DelayBox.Position = UDim2.new(0.690402508, 0, 0.126890033, 0)
-DelayBox.Size = UDim2.new(0, 84, 0, 42)
-DelayBox.Font = Enum.Font.SourceSans
-DelayBox.Text = "0.7"
-DelayBox.TextColor3 = Color3.fromRGB(255, 255, 255)
-DelayBox.TextScaled = true
-DelayBox.TextSize = 14.000
-DelayBox.TextWrapped = true
-
-minimze.Name = "minimze"
-minimze.Parent = Frame
-minimze.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-minimze.BorderColor3 = Color3.fromRGB(255, 255, 255)
-minimze.Position = UDim2.new(0.680000007, 0, 0, 0)
-minimze.Size = UDim2.new(0, 51, 0, 23)
-minimze.Font = Enum.Font.Jura
-minimze.Text = "-"
-minimze.TextColor3 = Color3.fromRGB(0, 0, 0)
-minimze.TextSize = 25.000
-
--- Scripts:
-
-local function HDPTQN_fake_script() -- X.LocalScript 
-	local script = Instance.new('LocalScript', X)
-
-	script.Parent.MouseEnter:Connect(function()
-		script.Parent.BackgroundColor3 = Color3.fromRGB(200, 0, 8)
-		script.Parent.BorderColor = Color3.fromRGB(200, 0, 8)
-		script.Parent.TextColor3 = Color3.fromRGB(255, 255, 255)
-	end)
-	script.Parent.MouseLeave:Connect(function()
-		script.Parent.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		script.Parent.BorderColor = Color3.fromRGB(255, 255, 255)
-		script.Parent.TextColor3 = Color3.fromRGB(0, 0, 0)
-	end)
+script_time = os.time()
+if not isfolder(".teus/BloxFruit/Kaitun/") then -- Mudança para "teus"
+               makefolder(".teus/BloxFruit/Kaitun/") -- Mudança para "teus"
 end
-coroutine.wrap(HDPTQN_fake_script)()
-local function CDWNJ_fake_script() -- X.LocalScript 
-	local script = Instance.new('LocalScript', X)
+inv = game:GetService("ReplicatedStorage").Remotes["CommF_"]:InvokeServer("getInventory")
 
-	local btn = script.Parent
-	btn.MouseButton1Click:Connect(function()
-		script.Parent.Parent.Parent:Destroy()
-	end)
+function randomChar()
+
+    local charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#₫_&-+()/*':;;!?£~`€|$•¢√^π°÷=×{§}∆%©®™✓[]'"
+
+    local length = 15
+    local index = math.random(1, length)
+    local char = charset:sub(index, index)
+    return char
 end
-coroutine.wrap(CDWNJ_fake_script)()
-local function DJZDV_fake_script() -- ChatBtn.LocalScript 
-	local script = Instance.new('LocalScript', ChatBtn)
 
-	local txtbox = script.Parent.Parent.Chat
-local delay = script.Parent.Parent.DelayBox
-local btn = script.Parent
-btn.MouseButton1Click:Connect(function()
-    local keywords = { "ass", "cum", "cock", "fuck", "asshole", "rape", "boobs", "titties", "sex", "hoe", "porn", "nigga", "penis", "beaner", "thot", "xxx", "hentai", "bitch", "bitches", "shit", "kys", "butthole", "masturbate", "big black cock", "blowjob", "bites your cock", "cock sucker", "fat ass hoe" } --Keyword
-    local replace = { "а⁥⁥⁥⁥ѕѕ", "с⁥⁥⁥⁥um", "сосk", "fu𐌑𝖔 ͜ ͣ c𐌑ḳ", "аѕѕһоlе", "rаре", "Вoobs", "Тitties", "s⁥⁥⁥⁥⁥⁥ех", "һое", "рогn", "п⁥iɡ⁥ɡ⁥а", "реп⁥is", "bеаn⁥ег", "ТН⁥⁥⁥⁥⁥ОТ", "ΧΧΧ", "НЕΝТАӀ", "ВI⁥⁥⁥⁥⁥⁥TСН", "ВIT⁥⁥⁥⁥⁥⁥⁥СНEЅ", "ЅНIТ", "К⁥⁥⁥⁥⁥YЅ", "ВUТТ⁥⁥⁥⁥НOLЕ", "МАЅТURВАТЕ", "big black со⁥⁥⁥сk", "Вӏоwjob", "Віtеѕ уоur сосk", "сосk suсkеr", "f⁥аt а⁥ѕѕ һое" } --Replace
-    local message = string.lower(txtbox.Text)
-    for word in string.gmatch(message, "%S+") do
-        for i, key in ipairs(keywords) do
-            if word == string.lower(key) then
-                word = replace[i]
-                break
-            end
-        end
-        game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(word, "All")
-        wait(tonumber(delay.Text))
-    end
-end)
+function getServerTime()
+    RealTime = tostring(math.floor(game.Lighting.ClockTime * 100) / 100)
+    RealTime = tostring(game.Lighting.ClockTime)
+    RealTimeTable = RealTime:split(".")
+    Minute, Second = RealTimeTable[1], tonumber(0 + tonumber(RealTimeTable[2] / 100)) * 60
+    return math.floor(Minute).."min"
 end
-coroutine.wrap(DJZDV_fake_script)()
-local function YKADJY_fake_script() -- Frame.LocalScript 
-	local script = Instance.new('LocalScript', Frame)
+local badtimedicac = os.time()
+if not isfile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name) then -- Mudança para "teus"
+    writefile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name, "0") -- Mudança para "teus"
+    esltime = 0 
+end 
+ctent2 = readfile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name) -- Mudança para "teus"
+function updatetime()
+    if not isfile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name) then -- Mudança para "teus"
+        writefile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name, "0") -- Mudança para "teus"
+        esltime = 0
+    else
+        ctent = readfile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name) -- Mudança para "teus"
+        if tonumber(ctent) or ctent == "0" or ctent == "1" then
+            esltime = tonumber(ctent2 or 0) + (os.time()-(badtimedicac or os.time()))
+            writefile(".teus/BloxFruit/Kaitun/eslapedtime."..game.Players.LocalPlayer.Name, tostring(esltime)) -- Mudança para "teus"
+            pcall(function() clockm:SetText(disp_time(esltime))end)
+            pcall(function() TextLabel_7.Text = getServerTime() end)
 
-	script.Parent.Draggable = true
-	script.Parent.Active = true
-end
-coroutine.wrap(YKADJY_fake_script)()
-local function AYOPMNY_fake_script() -- minimze.LocalScript 
-	local script = Instance.new('LocalScript', minimze)
-
-	local gui = script.Parent.Parent.Frame
-	local btn =script.Parent
-	btn.MouseButton1Click:Connect(function()
-		if gui.Visible == true then
-			gui.Visible = false
-			gui.Active = false
-		else
-			gui.Visible = true
-			gui.Active = true
-		end
-	end)
-end
-coroutine.wrap(AYOPMNY_fake_script)()
-
+        else
+            noti("Something Wrong With Config, Automatically Generate A New One")
+            esltime = "0"
+            writefile(".teus/BloxFruit/Kaitun/eslapedtime
