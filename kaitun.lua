ee180930db3aaa670dc6947d29ffdadf13c660bf
Teus-Scripts <mateusboaventura779@gmail.com>
2024-11-05 15:20:22 -0300

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,23 +1,3 @@
-for i,v in next, workspace:GetDescendants() do
-    pcall(function()
-        v.Transparency = 1
-    end)
-end
-for i,v in next, getnilinstances() do
-    pcall(function()
-        v.Transparency = 1
-        for i1,v1 in next, v:GetDescendants() do
-            v1.Transparency = 1
-        end
-    end)
-end
-a = workspace
-a.DescendantAdded:Connect(function(v)
-    pcall(function()
-        v.Transparency = 1
-    end)
-end)
-
 -- Max level, godhuman, CDK, soul guitar
 getgenv().Configs = {
     ["Team"] = "Pirates",
@@ -33,17 +13,5 @@ getgenv().Configs = {
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/refs/heads/main/kaitun.lua"))()
 
--- Configuração
-local mensagem = ".GG/TEUSCOMMUNITY"
-local tempo = 30 -- segundos
-
--- Função para enviar mensagem
-local function enviarMensagem()
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(mensagem, "All")
-end
-
--- Loop para enviar mensagem a cada 30 segundos
-while true do
-    wait(tempo)
-    enviarMensagem()
-end
+--remove map and auto chat
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Teus-Scripts/teste/refs/heads/main/teste.lua"))()
