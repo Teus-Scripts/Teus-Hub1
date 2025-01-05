4d2649840eeffb55a75c4a21cc77fce01d427317
Teus-Scripts <mateusboaventura779@gmail.com>
2025-01-05 05:36:58 -0300

    Update sla.lua

--- sla.lua
+++ sla.lua
@@ -3,30 +3,39 @@ _G.fps = 120
 _G.Mode = true
 loadstring(game:HttpGet('https://raw.githubusercontent.com/AloneBiNgu/AloneHub/main/lag'))()
 
--- Max level, godhuman
+-- Max level, godhuman, cdk, sgt , getkey
+script_key = "U6GJTMMIV6XLH2FX433O77X2ZLU7D57J7XF35KNXXI"
+getgenv().Shutdown = false -- Turn on if u are farming bulk accounts
 getgenv().Configs = {
-    ["Team"] = "Pirates",
-    ["Gun Farm"] = false,
+    ["Team"] = "Marines",
+    ["Gun Farm"] = false, -- Disabled
     ["FPS Boost"] = {
-        ["Enable"] = true,
-        ["FPS Cap"] = 120,
+        ["Enable"] = false,
+        ["FPS Cap"] = 30,
     },
     ["Farm Boss Drops"] = {
         ["Enable"] = false,
         ["When x2 Exp Expired"] = false
     },
+    ["Rainbow Haki"] = false,
     ["Hop Player Near"] = false,
-    ["Soul Guitar"] = false,
-    ["Find Fruit"] = true, -- Will find 1m+ fruit to unlock swan door to access third sea
-    ["Cursed Dual Katana"] = false
+    ["Skull Guitar"] = false,
+    ["Find Fruit"] = false, -- Will find 1m+ fruit to unlock swan door to access third sea
+    ["Cursed Dual Katana"] = false,
+    ["Switch Melee"] = true,  -- farm all melee
+    ["Eat Fruit"] = "", -- leave blank for none, put the fruit name like this example: Smoke Fruit, T-Rex Fruit, ...
+    ["Snipe Fruit"] = "", -- leave blank for none, put the fruit name like this example: Smoke Fruit, T-Rex Fruit, ...
+    ["Lock Fragment"] = 10000,
+    ["Buy Stuffs"] = true -- buso, geppo, soru, ken haki
 }
-repeat task.wait() pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/verudous/Xero-Hub/refs/heads/main/kaitun.lua"))() end) until getgenv().Check_Execute
+repeat task.wait() pcall(function() loadstring(game:HttpGet("https://reviewphim.fun/Xero%20Hub/Blox%20Fruit/kaitun.lua"))() end) until getgenv().Check_Execute
 
 local CoreGui = game:GetService("StarterGui")
 CoreGui:SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
 CoreGui:SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
 
-local mensagem = ".g g/teusco m m u n i t y"
+local mensagem = "Auto farm and free acc?
+Check this out: dc/YsR6ymwND4"
 local tempo = 100
 
 local function enviarMensagem()
