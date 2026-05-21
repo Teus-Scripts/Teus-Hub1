159cbe8bba095f54760783c599a273c1579bd241
Teus-Scripts <mateusboaventura779@gmail.com>
2026-05-20 23:28:18 -0300

    Update autobounty.lua

--- autobounty.lua
+++ autobounty.lua
@@ -3,52 +3,6 @@ _G.fps = 15
 _G.Mode = true
 loadstring(game:HttpGet('https://raw.githubusercontent.com/AloneBiNgu/AloneHub/main/lag'))()
 
-local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-local screenGui = Instance.new("ScreenGui")
-local textLabel = Instance.new("TextLabel")
-
-screenGui.Parent = game.CoreGui
-screenGui.DisplayOrder = 100
-
-textLabel.Parent = screenGui
-textLabel.Size = UDim2.new(0, 300, 0, 50)
-textLabel.Position = UDim2.new(0, 10, 0, 10) 
-textLabel.Font = Enum.Font.FredokaOne 
-textLabel.TextScaled = true 
-textLabel.BackgroundTransparency = 1 
-textLabel.TextStrokeTransparency = 0
-
-local function rainbowColor()
-    local Dreamon = 0
-    while true do
-        Dreamon = Dreamon + 0.01
-        if Dreamon > 1 then Dreamon = 0 end
-        textLabel.TextColor3 = Color3.fromHSV(Dreamon, 1, 1) 
-        RunService.RenderStepped:Wait()
-    end
-end
-
-local frameCount = 0
-local lastUpdate = tick()
-
-RunService.RenderStepped:Connect(function()
-    frameCount = frameCount + 1
-    local now = tick()
-
-    if now - lastUpdate >= 1 then
-        local fps = frameCount / (now - lastUpdate)
-        frameCount = 0
-        lastUpdate = now
-
-        local userName = LocalPlayer.Name
-        textLabel.Text = string.format("%s, FPS: %d",userName, math.floor(fps))
-    end
-end)
-
-spawn(rainbowColor)
 
 script_key = "U3VLB2EKRGQJJCVX433PZM5HXGYA" -- Put ur key between ""
 getgenv().Shutdown = true -- Turn on if u are farming bulk accounts
@@ -111,51 +65,3 @@ getgenv().Configs = {
     ["Buy Stuffs"] = true -- buso, geppo, soru, ken haki, ...
 }
 repeat task.wait(0.01) pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/kaitun.lua"))() end) until getgenv().Check_Execute
-
-
-local RunService = game:GetService("RunService")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-
-local screenGui = Instance.new("ScreenGui")
-local textLabel = Instance.new("TextLabel")
-
-screenGui.Parent = game.CoreGui
-screenGui.DisplayOrder = 100
-
-textLabel.Parent = screenGui
-textLabel.Size = UDim2.new(0, 300, 0, 50)
-textLabel.Position = UDim2.new(0, 10, 0, 10) 
-textLabel.Font = Enum.Font.FredokaOne 
-textLabel.TextScaled = true 
-textLabel.BackgroundTransparency = 1 
-textLabel.TextStrokeTransparency = 0
-
-local function rainbowColor()
-    local Dreamon = 0
-    while true do
-        Dreamon = Dreamon + 0.01
-        if Dreamon > 1 then Dreamon = 0 end
-        textLabel.TextColor3 = Color3.fromHSV(Dreamon, 1, 1) 
-        RunService.RenderStepped:Wait()
-    end
-end
-
-local frameCount = 0
-local lastUpdate = tick()
-
-RunService.RenderStepped:Connect(function()
-    frameCount = frameCount + 1
-    local now = tick()
-
-    if now - lastUpdate >= 1 then
-        local fps = frameCount / (now - lastUpdate)
-        frameCount = 0
-        lastUpdate = now
-
-        local userName = LocalPlayer.Name
-        textLabel.Text = string.format("%s, FPS: %d",userName, math.floor(fps))
-    end
-end)
-
-spawn(rainbowColor)
