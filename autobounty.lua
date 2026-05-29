2cb67a368afb67ccc510fbfb39078e2eea2c7a1e
Teus-Scripts <mateusboaventura779@gmail.com>
2026-05-28 23:06:10 -0300

    Update autobounty.lua

--- autobounty.lua
+++ autobounty.lua
@@ -1,46 +1,90 @@
-script_key = "U3VLB2EKRGQJJCVX433PZM5HXGYA" -- Put ur key between ""
-getgenv().Shutdown = false -- Turn on if u are farming bulk accounts
+-- FPS BOOST / REMOVE RENDER
+for i,v in next, workspace:GetDescendants() do
+    pcall(function()
+        v.Transparency = 1
+    end)
+end
+
+for i,v in next, getnilinstances() do
+    pcall(function()
+        v.Transparency = 1
+        for i1,v1 in next, v:GetDescendants() do
+            v1.Transparency = 1
+        end
+    end)
+end
+
+workspace.DescendantAdded:Connect(function(v)
+    pcall(function()
+        v.Transparency = 1
+    end)
+end)
+
+-- XERO HUB
+script_key = "U3VLB2EKRGQJJCVX433PZM5HXGYA"
+
+getgenv().Shutdown = true
+
 getgenv().Configs = {
     ["Team"] = "Marines",
+
     ["FPS Boost"] = {
         ["Enable"] = true,
-        ["FPS Cap"] = 60, --recommend 15
+        ["FPS Cap"] = 60,
     },
+
     ["Farm Boss Drops"] = {
         ["Enable"] = false,
         ["When x2 Exp Expired"] = false
     },
+
     ["Hop"] = {
         ["Enable"] = true,
         ["Hop Find Tushita"] = false,
         ["Hop Find Valkyrie Helm"] = false,
         ["Hop Find Mirror Fractal"] = false,
-        ["Hop Find Darkbeard"] = false, -- For skull guitar
-        ["Hop Find Soul Reaper"] = false, -- For CDK
-        ["Hop Find Mirage"] = false, -- For pull lever
-        ["Find Fruit"] = true, -- Will find 1m+ fruit to unlock swan door to access third sea
-        ["Hop Elite"] = false, -- For god chalice farming
+        ["Hop Find Darkbeard"] = false,
+        ["Hop Find Soul Reaper"] = false,
+        ["Hop Find Mirage"] = false,
+        ["Find Fruit"] = true,
+        ["Hop Elite"] = false,
     },
+
     ["Farm Mastery"] = {
         ["Enable"] = false,
-        ["Farm Mastery Weapons"] = {"Sword", "Gun", "Blox Fruit"}, -- Blox Fruit, Gun (left -> right: High -> Low Priority)
+        ["Farm Mastery Weapons"] = {"Sword", "Gun", "Blox Fruit"},
         ["Swords To Farm"] = {"Cursed Dual Katana"},
         ["Guns To Farm"] = {"Skull Guitar"},
-        ["Mastery Health (%)"] = 40 -- For Blox Fruit, Gun
+        ["Mastery Health (%)"] = 40
     },
+
     ["Farm Config"] = {
         ["First Farm At Sky"] = true,
+
         ["Farm Bone Get x2 Exp"] = {
             ["Enable"] = true,
-            ["Level"] = 1500 -- level to start farming
+            ["Level"] = 1500
         }
     },
+
     ["Trackstat"] = {
         ["Enable"] = true,
-        ["Key"] = "2", -- Get from xerohub.click
-        ["Device"] = "msi" -- u can put any name here
+        ["Key"] = "2",
+        ["Device"] = "msi"
     },
-    ["Fruit to use for auto third sea"] = {"Spider-Spider","Barrier-Barrier","Quake-Quake","Buddha-Buddha","Love-Love","Sound-Sound","Phoenix-Phoenix","Portal-Portal","Blizzard-Blizzard"}
+
+    ["Fruit to use for auto third sea"] = {
+        "Spider-Spider",
+        "Quake-Quake",
+        "Buddha-Buddha",
+        "Love-Love",
+        "Sound-Sound",
+        "Phoenix-Phoenix",
+        "Portal-Portal",
+        "Barrier-Barrier",
+        "Blizzard-Blizzard"
+    },
+
     ["Get Fruits"] = true,
     ["Auto Spawn rip_indra"] = false,
     ["Auto Spawn Dough King"] = false,
@@ -53,9 +97,15 @@ getgenv().Configs = {
     ["Skull Guitar"] = false,
     ["Cursed Dual Katana"] = false,
     ["Switch Melee"] = true,
-    ["Eat Fruit"] = "", -- leave blank for none, put the fruit name like this example: Smoke Fruit, T-Rex Fruit, ...
-    ["Snipe Fruit"] = "", -- leave blank for none, put the fruit name like this example: Smoke Fruit, T-Rex Fruit, ...
+    ["Eat Fruit"] = "",
+    ["Snipe Fruit"] = "",
     ["Lock Fragment"] = 0,
-    ["Buy Stuffs"] = true -- buso, geppo, soru, ken haki, ...
+    ["Buy Stuffs"] = true
 }
-repeat task.wait(0.01) pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/kaitun.lua"))() end) until getgenv().Check_Execute
+
+repeat
+    task.wait(0.01)
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Xero2409/XeroHub/refs/heads/main/kaitun.lua"))()
+    end)
+until getgenv().Check_Execute
